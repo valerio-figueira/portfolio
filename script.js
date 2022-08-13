b1ab666bb9d210ada561040fdd9e2c414bdd6d48
valerio-figueira <j.valerio.figueira@gmail.com>
2022-08-13 16:33:06 -0300

    updating files

--- script.js
+++ script.js
@@ -80,3 +80,38 @@ li[9].innerText = "Rede de Computadores";
 /*
 p.innerText = "HTML5, CSS3, Javascript, Java, Redes LAN-WAN, Hardware, CSS Flexbox, CSS Grid Layout, Linux, Rede de Computadores";*/
 
+/*
+Para treinar um pouco a interface DOM, irei colocar a descrição dos projetos pelo próprio javascript
+*/
+const description = document.querySelectorAll(".post .description");
+
+description[0].textContent = "Meu primeiro projeto real, feito para o Instituto de Previdência de minha cidade. O site é todo responsivo, adaptável a qualquer formato de tela. Feito com HTML, CSS e Javascript.";
+
+description[1].textContent = "Este é um dos meus projetos fictícios, no qual me empenhei por boas horas para me aprimorar como desenvolvedor web. Gostei muito de tê-lo feito, utilizei várias propriedades CSS de estilização, e.g., absolute position, flex, grid, @mediascreen, efeito parallax, transition, etc.";
+
+description[2].textContent = "Meu primeiro site feito com efeito parallax. É um site simples, mas com diversas propriedades avançadas de CSS. Há nele um navigator para mobiles e PCs desktop.";
+
+description[3].textContent = "Este modelo de site, com a propriedade grid, contém três colunas de artigos que, sendo visíveis para PCs desktop e outras telas, se ajustam a uma ou duas colunas conforme o dispositivo mobile.";
+
+description[5].textContent = "Meu primeiro \"projetinho\", o qual tentei fazer com o maior carinho, apesar de ser apenas um modelo fictício. Aprendi muito sobre estruturação de sites com este primeiro projeto.";
+
+/*Turmalina*/
+const link = document.createElement("a");
+link.setAttribute("href", "https://www.rocketseat.com.br/");
+link.setAttribute("target", "_blank");
+link.textContent = "rocketseat";
+
+description[4].innerHTML = `Um simples modelo de site, o qual tentei fazer "imitando" o modelo de site da `;
+description[4].appendChild(link);
+description[4].innerHTML += ` do qual faço parte como estudante de programação.`;
+
+
+/*
+Um simples modelo de site, o qual tentei fazer "imitando" o modelo de site da <a href="https://www.rocketseat.com.br/" target="_blank" rel="external">rocketseat</a>, do qual faço parte como estudante de programação.
+*/
+
+
+
+/*About myself*/
+const intro = document.querySelector(".paragraph");
+intro.innerHTML = "Desenvolvedor web, frontend. Estudante de HTML, CSS, JavaScript e outras tecnologias. Além disso, tenho 8 meses de experiência com Java, o qual aprendi resolvendo diversas listas de exercícios para aprimoramento da lógica de programação.";

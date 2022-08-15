be6a618b2419ec17da8d88c0b51da43753b6379f
valerio-figueira <j.valerio.figueira@gmail.com>
2022-08-15 01:55:30 -0300

    updating code

--- script.js
+++ script.js
@@ -85,30 +85,6 @@ Para treinar um pouco a interface DOM, irei colocar a descrição dos projetos p
 */
 const description = document.querySelectorAll(".post .description");
 
-description[0].textContent = "Meu primeiro projeto real, feito para o Instituto de Previdência de minha cidade. O site é todo responsivo, adaptável a qualquer formato de tela. Feito com HTML, CSS e Javascript.";
-
-description[1].textContent = "Este é um dos meus projetos fictícios, no qual me empenhei por boas horas para me aprimorar como desenvolvedor web. Gostei muito de tê-lo feito, utilizei várias propriedades CSS de estilização, e.g., absolute position, flex, grid, @mediascreen, efeito parallax, transition, etc.";
-
-description[2].textContent = "Meu primeiro site feito com efeito parallax. É um site simples, mas com diversas propriedades avançadas de CSS. Há nele um navigator para mobiles e PCs desktop.";
-
-description[3].textContent = "Este modelo de site, com a propriedade grid, contém três colunas de artigos que, sendo visíveis para PCs desktop e outras telas, se ajustam a uma ou duas colunas conforme o dispositivo mobile.";
-
-description[5].textContent = "Meu primeiro \"projetinho\", o qual tentei fazer com o maior carinho, conquanto seja apenas um modelo fictício. Aprendi muito sobre estruturação de sites com este primeiro projeto.";
-
-/*Turmalina*/
-const link = document.createElement("a");
-link.setAttribute("href", "https://www.rocketseat.com.br/");
-link.setAttribute("target", "_blank");
-link.textContent = "rocketseat";
-
-description[4].innerHTML = `Um simples modelo de site, o qual tentei fazer "imitando" o modelo de site da `;
-description[4].appendChild(link);
-description[4].innerHTML += `, do qual faço parte como estudante de programação.`;
-
-
-/*
-Um simples modelo de site, o qual tentei fazer "imitando" o modelo de site da <a href="https://www.rocketseat.com.br/" target="_blank" rel="external">rocketseat</a>, do qual faço parte como estudante de programação.
-*/
 
 
 
@@ -117,36 +93,18 @@ const intro = document.querySelector(".paragraph");
 intro.innerHTML = "Desenvolvedor web, frontend. Estudante de HTML, CSS, JavaScript e outras tecnologias. Além disso, tenho 8 meses de experiência com Java, o qual aprendi resolvendo diversas listas de exercícios para aprimoramento da lógica de programação.";
 
 
-/*
-LINKS INSIDE THE ARTICLE
-*/
-const post = document.querySelectorAll(".description");
-
-post[0] = createAnchor(post[0], "https://www.ipmamonte.mg.gov.br/");
-post[1] = createAnchor(post[1], "https://valerio-figueira.github.io/ruby-project/");
-post[2] = createAnchor(post[2], "https://valerio-figueira.github.io/jasper-project/");
-post[3] = createAnchor(post[3], "https://valerio-figueira.github.io/black-onyx-page/");
-post[4] = createAnchor(post[4], "https://valerio-figueira.github.io/turmalina-page/");
-post[0] = createAnchor(post[5], "https://valerio-figueira.github.io/lapis-lazuli-page/");
-
-function createAnchor(post, url){
-    let link = document.createElement("a");
-    link.setAttribute("href", url);
-    link.setAttribute("target", "_blank");
-    link.setAttribute("rel", "external");
-    link.innerHTML = " Preview";
-    post.appendChild(link);
-    return post;
-}
 
 
 
 /*ADD EFFECT CLASS IN IMGs*/
-
+const post = pullAllElements(".post .description");
 for(let item of post){
-    item.previousElementSibling.className = "img-effect";    
+    item.previousElementSibling.className = "img-effect";
 }
+
 /*
+TEXT INSIDE IMG
+
 let imgs = document.querySelectorAll(".img-effect");
 for(let img of imgs){
     createParagraphs(img);
@@ -163,3 +121,88 @@ function createParagraphs(img){
     return img;
 }
 */
+
+function createAnchorWithAppend(text, url, description){
+    let link = document.createElement("a");
+    link.setAttribute("href", url);
+    link.setAttribute("target", "_blank");
+    link.setAttribute("rel", "external");
+    link.innerHTML = description;
+    text.appendChild(link);
+    return text;
+}
+function createSimpleAnchor(url, description){
+    let link = document.createElement("a");
+    link.setAttribute("href", url);
+    link.setAttribute("target", "_blank");
+    link.setAttribute("rel", "external");
+    link.innerHTML = description;
+    return link;
+}
+function pullElement(id){
+    return document.querySelector(`${id}`);
+}
+function pullAllElements(id){
+    return document.querySelectorAll(`${id}`);
+}
+
+
+
+const linkText = " Preview";
+const ipma = pullElement("#ipma .description");
+ipma.innerHTML = "Meu primeiro projeto real, feito para o Instituto de Previdência de minha cidade. O site é todo responsivo, adaptável a qualquer formato de tela. Feito com HTML, CSS e Javascript.";
+createAnchorWithAppend(ipma, "https://www.ipmamonte.mg.gov.br/", linkText);
+
+const ruby = pullElement("#ruby .description");
+ruby.innerHTML = "Este é um dos meus projetos fictícios, no qual me empenhei por boas horas para me aprimorar como desenvolvedor web. Gostei muito de tê-lo feito, utilizei várias propriedades CSS de estilização, e.g., absolute position, flex, grid, @mediascreen, efeito parallax, transition, etc."
+createAnchorWithAppend(ruby, "https://valerio-figueira.github.io/ruby-project/", linkText);
+
+const jasper = pullElement("#jasper .description");
+jasper.innerHTML = "Meu primeiro site feito com efeito parallax. É um site simples, mas com diversas propriedades avançadas de CSS. Há nele um navigator para mobiles e PCs desktop.";
+createAnchorWithAppend(jasper, "https://valerio-figueira.github.io/jasper-project/", linkText);
+
+const blackOnyx = pullElement("#black-onyx .description");
+blackOnyx.innerHTML = "Este modelo de site, com a propriedade grid, contém três colunas de artigos que, sendo visíveis para PCs desktop e outras telas, se ajustam a uma ou duas colunas conforme o dispositivo mobile.";
+createAnchorWithAppend(blackOnyx, "https://valerio-figueira.github.io/black-onyx-page/", linkText);
+
+const lapisLazuli = pullElement("#lapis-lazuli .description");
+lapisLazuli.innerHTML = "Meu primeiro \"projetinho\", o qual tentei fazer com o maior carinho, conquanto seja apenas um modelo fictício. Aprendi muito sobre estruturação de sites com este primeiro projeto.";
+createAnchorWithAppend(lapisLazuli, "https://valerio-figueira.github.io/lapis-lazuli-page/", linkText);
+
+
+
+
+setAnchorAtributes(pullAllElements("#ipma a"), "https://www.ipmamonte.mg.gov.br/");
+setAnchorAtributes(pullAllElements("#subscription-page a"), "https://valerio-figueira.github.io/singlepage-subscription/");
+setAnchorAtributes(pullAllElements("#ruby a"), "https://valerio-figueira.github.io/ruby-project/");
+setAnchorAtributes(pullAllElements("#jasper a"), "https://valerio-figueira.github.io/jasper-project/");
+setAnchorAtributes(pullAllElements("#black-onyx a"), "https://valerio-figueira.github.io/black-onyx-page/");
+setAnchorAtributes(pullAllElements("#lapis-lazuli a"), "https://valerio-figueira.github.io/lapis-lazuli-page/");
+
+
+
+/*TURMALINA*/
+setAnchorAtributes(pullAllElements("#turmalina a"), "https://valerio-figueira.github.io/turmalina-page/");
+const turmalina = pullElement("#turmalina .description");
+turmalina.innerHTML = `Um simples modelo de site, o qual tentei fazer "imitando" o modelo de site da`;
+turmalina.appendChild(createSimpleAnchor("https://www.rocketseat.com.br/", " rocketseat"));
+turmalina.innerHTML += `, do qual faço parte como estudante de programação.`;
+createAnchorWithAppend(turmalina, "https://valerio-figueira.github.io/turmalina-page/", linkText);
+
+
+
+
+function setAnchorAtributes(elements, url){
+    for(let element of elements){
+        element.setAttribute("href", url);
+        element.setAttribute("target", "_blank");
+        element.setAttribute("rel", "external");
+    }
+    return elements;
+}
+
+const subscriptionPage = pullElement("#subscription-page .description");
+subscriptionPage.innerHTML = "Esta é uma página de cadastro, com valor de venda do produto por mensalidade. Feita com HTML e CSS apenas. A página é responsiva, se adaptando muito bem a qualquer dispositivo.";
+createAnchorWithAppend(subscriptionPage, "https://valerio-figueira.github.io/singlepage-subscription/", linkText);
+
+

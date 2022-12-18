76d093f19e1d7b53670924580fb18d925760027e
valerio-figueira <j.valerio.figueira@gmail.com>
2022-12-18 19:39:49 -0300

    version 1.0.0

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
-const URL = "https://portfolium-api.netlify.app/projects";
+const URL = "";
 // https://portfolium-api.netlify.app/projects
 
-
-async function fetchAPI() {
+async function fetchAPI(URL) {
     const loader = document.querySelector(".loader");
 
     loader.style.display = "block";
@@ -28,7 +27,7 @@ displayProjects(startIndex, endIndex);
 async function displayProjects(start, end){
     const portfolium = document.querySelector(".portfolium");
 
-    const projects = await fetchAPI();
+    const projects = await fetchAPI(URL);
 
     const html = projects.slice(start, end).map(project => {
         return `
@@ -50,8 +49,10 @@ async function displayProjects(start, end){
             <a href="${project.url}" target="blank" class="knowmore">Saiba mais <span class="fa fa-angle-double-right icon"></span></a>
         </article>
         `;
-    }).join("");    
+    }).join(""); 
+
     btnController(start, end, projects);
+
     portfolium.innerHTML = html;
 
     addEvents()
@@ -171,3 +172,142 @@ document.querySelector('footer .up-btn')
 .addEventListener('click', () => {
     window.open('#home', '_self');
 });
+
+
+
+/* ABILITIES LINK & POPUP*/
+document.querySelectorAll('.abilities li')
+.forEach(li => {
+    li.addEventListener('click', (e) => {
+        openPopup(e.target);
+    })
+})
+
+document.querySelector('.close-popup')
+.addEventListener('click', (e) => {
+    getConditional(e.target.parentNode);
+})
+
+// IF ESC IS PRESSED
+document.addEventListener("keydown", (e) => {
+    if(e.key.match("Escape")){
+        getConditional(document.querySelector(".popup"));
+    }
+})
+
+async function openPopup(element){
+    const popup = document.querySelector('.popup');
+
+    getConditional(popup);
+
+    // async function and search conditionals
+    const technologies = await fetchTechnologies(`http://localhost:8888/technologies/tag/${element.id}`);
+
+    if(technologies != undefined){
+        createElements(technologies);
+    }
+}
+
+function getConditional(element){
+    const main = document.querySelector('main');
+
+    if(element.matches('.open')){
+        element.classList.remove('open');
+        console.log(element.classList)
+        document.body.style.overflow = "scroll";
+        main.style.filter = "blur(0)";
+        document.querySelector(".popup-content").innerHTML = "";
+        document.querySelector(".close-popup").style.display = "none";
+    } else{
+        element.classList.add('open');
+        document.body.style.overflow = "hidden";
+        main.style.filter = "blur(3px)";
+        document.querySelector(".close-popup").style.display = "block";
+    }
+}
+
+async function fetchTechnologies(URL){
+    const spinner = document.querySelector('.loader.popup');
+    spinner.style.display = "block";
+
+    return await fetch(URL)
+    .then(response => {
+        return Promise.resolve(response);
+    })
+    .then(data => {
+        return data.json();
+    })
+    .then(technologies => {
+        spinner.style.display = "none";
+        return technologies;
+    })
+}
+
+
+function createElements(technologies){
+    const {title, content, references, images, date} = technologies;
+
+    const container = document.querySelector(".popup-content");
+
+    const h2 = document.createElement("h2");
+    const img = document.createElement("img");
+    const small = document.createElement("small");
+    const div = document.createElement("div");
+    
+    // SET CLASSES NAME OF EACH SINGLE TAG
+    h2.className = "title";
+    img.className = "image";
+    small.className = "date";
+
+    // SET TITLE AND IMAGES ATTRIBUTES
+    h2.innerHTML = title;
+    img.setAttribute("src", images[0]);
+    img.setAttribute("alt", title);
+
+    // WARN THE USER ABOUT ESC KEY TO EXIT
+    const warningTag = document.createElement("small");
+    warningTag.className = "warning";
+    warningTag.innerHTML = "Press Esc to Exit";
+
+    container.appendChild(warningTag);
+
+    const titleContainer = document.createElement("div");
+    titleContainer.className = "title-container";
+    titleContainer.appendChild(h2);
+    titleContainer.appendChild(img);
+    container.appendChild(titleContainer);
+
+    // CREATE ALL PARAGRAPHS TAGS AND ADD INTO CONTAINER
+    for(let paragraph of content){
+        let paragraphTag = document.createElement("p");
+        paragraphTag.className = "paragraph";
+        paragraphTag.innerHTML = paragraph;
+        div.appendChild(paragraphTag);
+    }
+
+    // PULL DIV TO BE THE CONTAINER OF ALL PARAGRAPHS
+    div.className = "paragraphs";
+    container.appendChild(div);
+
+    // CREATE ALL REFERENCES ANCHORS AND ADD ALL THEIR ATTRIBUTES
+    const linksContainer = document.createElement("div");
+    linksContainer.className = "references";
+    linksContainer.innerHTML = "References: ";
+
+    for(let reference of references){
+        const referenceTag = document.createElement("a");
+        referenceTag.href = reference;
+        referenceTag.target = "_blank";
+        referenceTag.rel = "external";
+        referenceTag.className = "reference";
+        referenceTag.innerHTML = reference;
+        linksContainer.appendChild(referenceTag);
+    }
+    container.appendChild(linksContainer);
+
+    // INSERT DATETIME TAG INTO THE CONTAINER
+    small.innerHTML = `Published: ${new Date(date).toLocaleDateString()}`;
+    container.appendChild(small);
+    
+
+}
